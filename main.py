32a2d81793fef2c1d5d935d36e358395342caed4
AaryaBC <aaryabc49@gmail.com>
2018-12-06 06:11:08 -0500

    Part A and B

---
+++ main.py
@@ -0,0 +1,42 @@
+import csv
+import json
+import requests
+import pandas
+import matplotlib.pyplot as plt
+
+## Global data that we might need later. Will change this whole thing into a class later, this is to receive the results quickly at first
+YEARS = []
+VALUES = []
+
+def getData():
+	with requests.Session() as s:
+		year = 1989
+		dataFile = open('data.csv', 'w')
+		count = 0
+		while year < 2019:
+			dataYear = str(year)
+			CSV_URL = 'https://quickstats.nass.usda.gov/api/api_GET/?key=AE577233-F40D-3716-BCC3-24DA66B479F1&commodity_desc=TURKEYS&short_desc=TURKEYS,%20YOUNG,%20SLAUGHTER,%20FI%20-%20SLAUGHTERED,%20MEASURED%20IN%20HEAD&year__GE=' +  dataYear + '&state_alpha=VA&format=JSON&param=short_desc'
+			download = s.get(CSV_URL)
+			decoded_content = download.content.decode('utf-8')
+			parsedData = json.loads(decoded_content)['data']
+			
+			writer = csv.writer(dataFile)
+			for vals in parsedData:
+				if count == 0:
+					header = vals.keys()
+					writer.writerow(header)
+					count += 1
+				writer.writerow(vals.values())
+			year += 1
+
+def readCSV():
+	df = pandas.read_csv('data.csv')
+	for year in df['year']:
+		YEARS.append(year)
+	for value in df['Value']:
+		VALUES.append( int(value.replace(',',''))/1000 )
+
+	plt.plot(YEARS, VALUES)
+	plt.show()
+
+readCSV()
